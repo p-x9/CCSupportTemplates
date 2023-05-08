eca920b73bea21e202cb6228360017e9d1de367d
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-05-09 00:07:11 +0900

    update script for install

--- install.sh
+++ install.sh
@@ -7,8 +7,13 @@ if [ -f "$THEOS/templates/ios/opa334/control_center_module.nic.tar" ]; then
     rm $THEOS/templates/ios/opa334/control_center_module.nic.tar
 fi
 
+$THEOS/bin/nicify.pl ccsupport_module_provider-11up
+$THEOS/bin/nicify.pl control_center_module-11up
+$THEOS/bin/nicify.pl control_center_module-swift
+
 cp ./iphone_control_center_module-11up.nic.tar $THEOS/templates/ios/opa334
 cp ./iphone_ccsupport_module_provider-11up.nic.tar $THEOS/templates/ios/opa334
+cp ./iphone_control_center_module_swift.nic.tar $THEOS/templates/ios/opa334
 
 cp -r ./Headers/ControlCenterUI $THEOS/include
 cp -r ./Headers/ControlCenterUIKit $THEOS/include
